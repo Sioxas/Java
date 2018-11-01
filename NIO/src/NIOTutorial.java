5e05a1badf27a183d221172e7997d2ddc4db2e97
zhsh <sioxas@qq.com>
2018-11-01 21:30:55 +0800

    Hello NIO

---
+++ NIO/src/NIOTutorial.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NIOTutorial {
+    public static void main(String[] args) {
+        try {
+            RandomAccessFile aFile = new RandomAccessFile("/Users/cherry/Development/Java/NIO/data/nio-data.txt", "rw");
+            FileChannel inChannel = aFile.getChannel();
+            ByteBuffer buf = ByteBuffer.allocate(10);
+            int bytesRead = inChannel.read(buf);
+            while (bytesRead != -1) {
+                System.out.println("Read" + bytesRead);
+                buf.flip();
+                while (buf.hasRemaining()) {
+                    System.out.print((char) buf.get());
+                }
+                buf.clear();
+                bytesRead = inChannel.read(buf);
+            }
+            aFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
